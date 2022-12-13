7e591d9b6cb406bff784d8eadfd5c71721958d00
JAMS23 <juanmena@MacBook-Pro-de-Juan.local>
2022-12-13 21:48:20 +0000

    arreglo tf.keras

--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,5 @@
 import numpy as np
-from keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.utils import load_img, img_to_array
 from keras.models import load_model
 
 def predict(file):
